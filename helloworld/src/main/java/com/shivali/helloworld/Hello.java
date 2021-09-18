4b6969169686b965e93d0476aeb67438dce1121d
Shivali Desai <shiv.kar@yahoo.com>
2021-09-18 20:58:02 +0530

    My netBeans Hello world project

---
+++ helloworld/src/main/java/com/shivali/helloworld/Hello.java
@@ -0,0 +1,23 @@
+
+/**
+ *@author The following has evaluated to null or missing:
+==> Shivali  [in template "Templates/Classes/Class.java" at line 3, column 13]
+
+----
+Tip: If the failing expression is known to legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
+----
+
+----
+FTL stack trace ("~" means nesting-related):
+	- Failed at: ${Shivali}  [in template "Templates/Classes/Class.java" at line 3, column 11]
+----
+ * email : shiv.kar@yahoo.com
+ * date : 08.09.2021
+ * purpose : My first Hello World program in an IDE
+ */
+
+package com.shivali.helloworld;
+
+public class Hello {public static void main(String[] args) {System.out.println("Hello World!");}
+
+}
