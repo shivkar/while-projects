527912a78b5a9cfbf3c73fdf039fb804948de7d8
shivkar <SHIVALI@192.168.1.106>
2021-09-27 11:13:31 -0700

    Java if/else project in NetBeans pickyeater

---
+++ Pickyeater/src/main/java/com/myproject/pickyeater/pickyeater.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.pickyeater;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class pickyeater {public static void main(String[] args) {
+        Scanner sk = new Scanner(System.in);
+     System.out.println("How many times has it been fried? (#) ");
+     int Timefrieds = Integer.parseInt(sk.nextLine());
+     System.out.println("Does it have any spinach in it? (y/n) ");
+     String Spinachin = sk.nextLine();
+     System.out.println("Does it have a funny name? (y/n) ");
+     String funnyname = sk.nextLine();
+     if(Spinachin .equals("y") || funnyname .equals("y"));
+       System.
+               out.print("There's no way he'll eat that! ");}
+      
+}
