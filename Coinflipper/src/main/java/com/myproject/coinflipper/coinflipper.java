404641ad1c8fdf44b59bdd2919a1a56e08ac0aa6
shivkar <SHIVALI@192.168.1.106>
2021-09-27 13:49:12 -0700

    Java random projects

---
+++ Coinflipper/src/main/java/com/myproject/coinflipper/coinflipper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.coinflipper;
+
+import java.util.Random;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class coinflipper {
+    public static void main(String[] args){
+   
+   
+             System.out.println("Ready, Set, Flip....!");
+             if(Math.random() > 1){System.out.println("You got TAILS!");}
+             else {System.out.println("You got HEAD!");}
+             
+}}
