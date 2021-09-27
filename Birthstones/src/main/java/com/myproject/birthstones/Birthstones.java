dc9eea1c8b031e4298f2c9eabbf570f5c8136318
shivkar <SHIVALI@192.168.1.106>
2021-09-27 09:08:27 -0700

    Java if/else project in NetBeans

---
+++ Birthstones/src/main/java/com/myproject/birthstones/Birthstones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.birthstones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class Birthstones {public static void main(String[] args){
+
+Scanner sk = new Scanner(System.in);
+    System.out.println("What month's birthstone do you want to know? " );
+   String January =  sk.nextLine();
+if(January .equals ("1")){System.out.println("January's is Birthstone of Garnett");}
+else {System.out.println(" I think you are confused " + January + " is wrong number ");}
+  }}
