94f92536a7f42875ca6a7749819e170116448a62
shivkar <SHIVALI@192.168.1.106>
2021-09-27 15:01:37 -0700

    Java while project

---
+++ Staypositive/src/main/java/com/myproject/staypositive/sp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.staypositive;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class sp {public static void main(String[] args) {
+      System.out.println("Counting down..");
+      int i = 10;
+    while (i > -1) {
+      System.out.println(i);
+      i--;}
+     System.out.println("Blast off");
+}}
