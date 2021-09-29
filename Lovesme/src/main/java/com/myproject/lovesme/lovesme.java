0acf4aa167f864f51bb584fb8760c720f51153af
shivkar <SHIVALI@192.168.1.106>
2021-09-29 06:56:21 -0700

    Javado while project

---
+++ Lovesme/src/main/java/com/myproject/lovesme/lovesme.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.lovesme;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class lovesme {public static void main(String[] args){
+   
+    System.out.println("Well here goes nothing... ");
+   
+     int petal = 34;
+     int lovesMe = 0;
+     
+   if(lovesMe < petal ){System.out.println("It loves me !");} else if ( lovesMe > petal){System.out.println("It loves me not!");}
+                   
+         do{System.out.println("It loves me not!");petal++;}
+        while (lovesMe == 34 );{System.out.println("It loves me !");} 
+     
+       {System.out.println("I knew it! It loves me!");}}
+       
+     
+       
+}
