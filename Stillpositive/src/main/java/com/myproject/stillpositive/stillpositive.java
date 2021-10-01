3214e64ee749eb4d9c686143cd32e5b6a26955bd
shivkar <SHIVALI@192.168.1.106>
2021-10-01 01:54:40 -0700

    Java array  project

---
+++ Stillpositive/src/main/java/com/myproject/stillpositive/stillpositive.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myproject.stillpositive;
+
+/**
+ *
+ * @author SHIVALI
+ */
+public class stillpositive {public static void main(String[] args){ 
+    int[] numbers = {389, -447, 26, -485, 712, -884, 94, -64, 868, -776, 227, -744, 422, -109, 259, -500, 278, -219, 799, -311};
+    int j = 0;
+    while ( j < numbers.length){if(numbers[j]>=0){System.out.format(" %d ", numbers[j]);}j++;
+    
+}}}
